db76b4d22bca8e7fb78e6f2cc7a79e7351d24328
Fede Otálvares <fnotalvares@gmail.com>
2023-07-01 10:46:31 +0200

    Add navigation component

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,18 @@
+const Nav = () => {
+  return (
+    <>
+      <nav className="nav">
+        <div className="nav__designer">
+          <a>About me</a>
+          <a>UX Designer</a>
+        </div>
+        <div className="nav__developer">
+          <a>Developer</a>
+          <a>Say Hi!</a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingPage = (): React.ReactElement => {
+  return <></>;
+};
+
+export default LandingPage;
